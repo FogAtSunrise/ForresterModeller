3071ded1953f7b2663b62a90557cbe03cc0f2c15
Верещагина Ирина Александровна <hjr462@mail.ru>
2022-03-04 15:51:50 +0700

    interface Command

---
+++ WPFtest1/src/Command/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFtest1.src.Command
+{
+    interface ICommand
+    {
+        void execute();
+        void undo();
+    }
+}
---
+++ WPFtest1/src/Command/IDiagramEdgeCommand/IDiagramEdgeCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFtest1.src.Command.IDiagramEdgeCommand
+{
+    interface IDiagramEdgeCommand : ICommand
+    {
+        // private Diagram diagram;
+       //private DiagramEdge edge;
+
+    }
+}
