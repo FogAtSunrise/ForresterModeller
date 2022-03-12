174494ef672e5b1bf0c7e7b4bfbcbaeb39bed343
Верещагина Ирина Александровна <hjr462@mail.ru>
2022-03-12 21:31:43 +0700

    Добавлены чекбоксы в дерево, для примера составлен наглядный список, чтобы список читался по реальным классам, надо раскомментировать код в методе ChangeListInPlotterTools

--- WPFtest1/src/Pages/Tools/PlotterTools.xaml.cs
+++ WPFtest1/src/Pages/Tools/PlotterTools.xaml.cs
@@ -25,65 +25,66 @@ namespace ForresterModeller.src.Pages.Tools
         /// абстрактному классу определяет в соответствующую группу
         /// </summary>
         /// <param name="elements"></param>
-        // public void ChangeListInPlotterTools(List<IDiagramEntity> elements)
+        
         public void ChangeListInPlotterTools(List<IDiagramEntity> elements, string name)
         {
-            //string name = "diagram1"+countColumn++;
             
- 
-            TreeViewItem treeHead = new TreeViewItem() { Header = name };
-                      TreeViewItem tree1 = new TreeViewItem() { Header = "Уровни" };
-                       TreeViewItem tree2 = new TreeViewItem() { Header = "Уравнения" };
 
-                       foreach (var elem in elements)
-                           if (elem is DiagramLevel)
-                               tree1.Items.Add(new TreeViewItem() { Header = elem.Name});
-                           else if (elem is DiagramFunction)
-                               tree2.Items.Add(new TreeViewItem() { Header = elem.Name });
+          /*  Node treeHead = new Node() { NodeName = name, Nodes = new List<Node>()};
+            Node tree1 = new Node() { NodeName = "Уровни",   Nodes = new List<Node>()};
+            Node tree2 = new Node() { NodeName = "Уравнения", Nodes = new List<Node>()};
+            foreach (var elem in elements)
+                if (elem is DiagramLevel)
+                    tree1.Nodes.Add(new Node() { NodeName = elem.Name, Nodes = new List<Node>()});
+                else if (elem is DiagramFunction)
+                    tree2.Nodes.Add(new Node() { NodeName = elem.Name, Nodes = new List<Node>() });
+            treeHead.Nodes.Add(tree1);
+            treeHead.Nodes.Add(tree2);
+            nodeList = new List<Node>() { treeHead};*/
+            nodeList=GetNodeList();
+            this.TreeView_NodeList.ItemsSource = nodeList;
+          
 
-                       treeHead.Items.Add(tree1);
-                       treeHead.Items.Add(tree2);
-               
- 
-            
-         //   TreeViewEvent.Items.Add(treeHead);
-
-         
+            ExpandTree();
         }
 
-
+        /// <summary>
+        /// дерево элементов
+        /// </summary>
         public List<Node> nodeList { get; set; }
         private List<Node> GetNodeList()
         {
             Node leafOneNode = new Node();
-            leafOneNode.NodeName = "Leaf Node One";
+            leafOneNode.NodeName = "Уровень1";
             leafOneNode.Nodes = new List<Node>();
 
             Node leafTwoNode = new Node();
-            leafTwoNode.NodeName = "Leaf Node Two";
+            leafTwoNode.NodeName = "Уровень2";
             leafTwoNode.Nodes = new List<Node>();
 
             Node leafThreeNode = new Node();
-            leafThreeNode.NodeName = "Leaf Node Three";
+            leafThreeNode.NodeName = "Уровень3";
             leafThreeNode.Nodes = new List<Node>();
 
             Node leafOneNode1 = new Node();
-            leafOneNode1.NodeName = "Leaf Node 1";
+            leafOneNode1.NodeName = "Уравнение1";
             leafOneNode1.Nodes = new List<Node>();
 
             Node leafTwoNode2 = new Node();
-            leafTwoNode2.NodeName = "Leaf Node 2";
+            leafTwoNode2.NodeName = "Уравнение2";
             leafTwoNode2.Nodes = new List<Node>();
 
             Node leafThreeNode3 = new Node();
-            leafThreeNode3.NodeName = "Leaf Node 3";
+            leafThreeNode3.NodeName = "Уравнение3";
             leafThreeNode3.Nodes = new List<Node>();
 
 
             return new List<Node>()
             {
-                new Node(){NodeName="Root Node",Nodes=new List<Node>(){leafOneNode, leafTwoNode, leafThreeNode}},
-                new Node(){NodeName="Root Node 1",Nodes=new List<Node>(){leafOneNode1, leafTwoNode2, leafThreeNode3}}
+                new Node(){NodeName="Файл1",Nodes=new List<Node>(){
+                new Node(){NodeName="Уровни",Nodes=new List<Node>(){leafOneNode, leafTwoNode, leafThreeNode}},
+                new Node(){NodeName="Уравнения",Nodes=new List<Node>(){leafOneNode1, leafTwoNode2, leafThreeNode3}}
+                } }
             };
         }
         private void ExpandTree()
@@ -101,30 +102,33 @@ namespace ForresterModeller.src.Pages.Tools
             }
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private Node findNode(string name, List<Node> node)
         {
-            nodeList = GetNodeList();
-            this.TreeView_NodeList.ItemsSource = nodeList;
-            ExpandTree();
-        }
+            Node ans = new Node();
+            foreach (Node item in node)
+                if (item.NodeName == name)
+                    return item;
+                else if (item.Nodes != null) ans = findNode(name, item.Nodes);
+                else return null;
+                
+            return ans;
+                }
 
+        private void checkNode(bool mean, Node node)
+        {   node.IsSelected = mean;
+            if(node.Nodes!=null)
+            foreach (Node item in node.Nodes)
+            {
+                    checkNode(mean, item);
+            }
+        }
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
-            if ((sender as CheckBox).Content.ToString() == "Root Node")
-            {
-                foreach (Node item in nodeList[0].Nodes)
-                {
-                    item.IsSelected = true;
-                }
-            }
-            else if ((sender as CheckBox).Content.ToString() == "Root Node 1")
-            {
-                foreach (Node item in nodeList[1].Nodes)
-                {
-                    item.IsSelected = true;
-                }
-            }
-            else
+           Node mynode = findNode((sender as CheckBox).Content.ToString(), nodeList);
+            if (mynode != null)
+                checkNode(true, mynode);
+
+       /*    else
             {
                 if (nodeList[0].Nodes.Exists(x => x.NodeName == (sender as CheckBox).Content.ToString()))
                 {
@@ -145,6 +149,7 @@ namespace ForresterModeller.src.Pages.Tools
                         nodeList[1].IsSelected = null;
                 }
             }
+       */
         }
 
 
@@ -152,42 +157,45 @@ namespace ForresterModeller.src.Pages.Tools
 
         private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
         {
-            if ((sender as CheckBox).Content.ToString() == "Root Node")
-            {
-                foreach (Node item in nodeList[0].Nodes)
-                {
-                    item.IsSelected = false;
-                }
-            }
-            else if ((sender as CheckBox).Content.ToString() == "Root Node 1")
-            {
-                foreach (Node item in nodeList[1].Nodes)
-                {
-                    item.IsSelected = false;
-                }
-            }
-            else
-            {
-                if (nodeList[0].Nodes.Exists(x => x.NodeName == (sender as CheckBox).Content.ToString()))
-                {
-                    if (nodeList[0].Nodes.All(x => x.IsSelected == false))
-                    {
-                        nodeList[0].IsSelected = false;
-                    }
-                    else
-                        nodeList[0].IsSelected = null;
-                }
-                if (nodeList[1].Nodes.Exists(x => x.NodeName == (sender as CheckBox).Content.ToString()))
-                {
-                    if (nodeList[1].Nodes.All(x => x.IsSelected == false))
-                    {
-                        nodeList[1].IsSelected = false;
-                    }
-                    else
-                        nodeList[1].IsSelected = null;
-                }
-            }
-        }
+            Node mynode = findNode((sender as CheckBox).Content.ToString(), nodeList);
+            if (mynode != null)
+                checkNode(false, mynode);
+            /* if ((sender as CheckBox).Content.ToString() == "Root Node")
+             {
+                 foreach (Node item in nodeList[0].Nodes)
+                 {
+                     item.IsSelected = false;
+                 }
+             }
+             else if ((sender as CheckBox).Content.ToString() == "Root Node 1")
+             {
+                 foreach (Node item in nodeList[1].Nodes)
+                 {
+                     item.IsSelected = false;
+                 }
+             }
+             else
+             {
+                 if (nodeList[0].Nodes.Exists(x => x.NodeName == (sender as CheckBox).Content.ToString()))
+                 {
+                     if (nodeList[0].Nodes.All(x => x.IsSelected == false))
+                     {
+                         nodeList[0].IsSelected = false;
+                     }
+                     else
+                         nodeList[0].IsSelected = null;
+                 }
+                 if (nodeList[1].Nodes.Exists(x => x.NodeName == (sender as CheckBox).Content.ToString()))
+                 {
+                     if (nodeList[1].Nodes.All(x => x.IsSelected == false))
+                     {
+                         nodeList[1].IsSelected = false;
+                     }
+                     else
+                         nodeList[1].IsSelected = null;
+                 }
+             }*/
 
+        }
     }
 }
