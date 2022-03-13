45536d15d387a53987320d5dc6c975746d916bba
OldKrab <4shushakov@gmail.com>
2022-03-13 19:27:38 +0700

    real merge

---
+++ ForresterModeller/src/Nodes/Models/FunkNodeModel.cs
@@ -0,0 +1,37 @@
+﻿using DynamicData;
+using NodeNetwork.ViewModels;
+using ReactiveUI;
+using WPFtest1.src.Nodes.Views;
+
+namespace WPFtest1.src.Nodes.Models
+{
+    public class FunkNodeModel : ForesterNodeModel
+    {
+        public string Funk { get; set; }
+        public FunkNodeModel(string name, string fulname, string funk) : base()
+        {
+            this.Name = name;
+            this.FullName = fulname;
+            this.Code = name;
+            this.Funk = funk;
+
+            var a = new NodeOutputViewModel();
+            a.PortPosition = PortPosition.Left;
+            this.Outputs.Add(a);
+
+            for (int i = 0; i < 5; i++)
+            {
+                var b = new NodeInputViewModel();
+                b.PortPosition = PortPosition.Circle;
+                Inputs.Add(b);
+            }
+        }
+        public FunkNodeModel() : this("LVL", "Уровень", "1") { }
+        static FunkNodeModel()
+        {
+            Splat.Locator.CurrentMutable.Register(() => new ForesterNodeView("funk"), typeof(IViewFor<FunkNodeModel>));
+        }
+    }
+
+
+}
---
+++ ForresterModeller/src/Windows/NodeWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using DynamicData;
+using NodeNetwork.ViewModels;
+using System.Windows;
+using WPFtest1.src.Nodes.Models;
+
+namespace WPFtest1
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class NodeWindow : Window
+    {
+        public NodeWindow()
+        {
+
+            InitializeComponent();
+            var network = new NetworkViewModel();
+
+
+            var node1 = new ConstantNodeViewModel();
+            network.Nodes.Add(node1);
+
+            var node2 = new LevelNodeModel();
+            network.Nodes.Add(node2);
+
+            var node3 = new FunkNodeModel();
+            network.Nodes.Add(node3);
+
+            networkView.ViewModel = network;
+        }
+
+        private void networkView_Loaded()
+        {
+
+        }
+    }
+}
