ee9c84a10132ab8b9b8b68dc29a90ec4265d8bb2
olga <bachisheo@gmail.com>
2022-03-14 10:05:50 +0700

    Исправлено форматирование

--- Charts/SwordFish.NET/TestPage.xaml.cs
+++ Charts/SwordFish.NET/TestPage.xaml.cs
@@ -9,33 +9,30 @@
 // - Change Log -                                                            **
 //*****************************************************************************
 
-using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Swordfish.NET.Charts {
-  /// <summary>
-  /// Interaction logic for Page1.xaml
-  /// </summary>
-  public partial class TestPage : Window {
-    public TestPage() {
-      InitializeComponent();
-      ChartUtilities.AddTestLines(xyLineChart);
-      xyLineChart.SubNotes = new string[] { "Right or Middle Mouse Button To Zoom, Left Mouse Button To Pan, Right Double-Click To Reset" };
-      copyToClipboard.DoCopyToClipboard = ChartUtilities.CopyChartToClipboard;
+namespace Swordfish.NET.Charts
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class TestPage : Window
+    {
+        public TestPage()
+        {
+            InitializeComponent();
+            ChartUtilities.AddTestLines(xyLineChart);
+            xyLineChart.SubNotes = new string[] { "Масштабирование колесом мыши, перемещение при нажатии левой кнопки мыши, отмена действий - двойной клик правой кнопкой" };
+            copyToClipboard.DoCopyToClipboard = ChartUtilities.CopyChartToClipboard;
+        }
+
+        public ChartControl XYLineChart
+        {
+            get
+            {
+                return xyLineChart;
+            }
+        }
+
     }
-
-    public ChartControl XYLineChart {
-      get {
-        return xyLineChart;
-      }
-    }
-
-  }
 }
