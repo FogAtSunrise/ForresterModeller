8511151c05a0d6bf2e2d4c4ec6ec4127fccc0c57
Верещагина Ирина Александровна <hjr462@mail.ru>
2022-03-20 12:36:49 +0700

    Перенесла график в таб,  TestPlot -открывает страницу с графиком, TestGraf  - открывает страницу с диаграммой

--- ForresterModeller/src/Windows/MainWindow.xaml.cs
+++ ForresterModeller/src/Windows/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Windows;
 using System.Windows.Controls;
 using DynamicData;
@@ -9,7 +10,15 @@ using ForresterModeller.src.Pages.Properties;
 using ForresterModeller.src.Pages.Tools;
 using ForresterModeller.src.Tools;
 using NodeNetwork.ViewModels;
+using NodeNetwork.Views;
+using System.Windows.Media;
 using WPFtest1.src.Nodes.Models;
+using Brushes = System.Windows.Media.Brushes;
+using ScottPlot.Plottable;
+using ScottPlot;
+
+
+
 
 namespace ForresterModeller
 {
@@ -18,22 +27,20 @@ namespace ForresterModeller
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string V = "AliceBlue";
+
         //  public Frame mainFrame { get; set; }
         public MainWindow()
         {
             InitializeComponent();
-
+           
             var network = new NetworkViewModel();
-
-
             var node1 = new ConstantNodeViewModel();
             network.Nodes.Add(node1);
-
             var node2 = new LevelNodeModel();
             network.Nodes.Add(node2);
-
-
             networkView.ViewModel = network;
+
 
             OpenProperty();
         }
@@ -133,7 +140,7 @@ namespace ForresterModeller
         {
             TreeViewItem item = sender as TreeViewItem;
           //  MessageBox.Show("Должен открыться " + item.Header);
-            openNewPage((string)item.Header);
+            openNewPage((string)item.Header, "plotter");
 
         }
 
@@ -143,30 +150,42 @@ namespace ForresterModeller
             
         }
 
-        /*
-         * TabItem  Header="Tab1" Width="150" Height="20">
-         *   <TabItem.HeaderTemplate >
-                                <DataTemplate>
-                                    <Canvas  Height="20" Width="140" >
-                                        <TextBlock Text="TabItem 1" Canvas.Left="0" Canvas.Bottom="2"  Width="120" />
-                                        <Button Background="White" Content="x" Click="ButtonBase_OnClick"  Canvas.Right="0" Height="18" Width="20"  Canvas.Left="120" />
-                                    </Canvas>
-                                </DataTemplate>
-                            </TabItem.HeaderTemplate>
-         */
+        public void FillPlot(WpfPlot WpfPlot1)
+        {
+            InitializeComponent();
+            double[] dataX = { 1, 2, 3, 4, 5 };
+            double[] dataY = { 1, 4, 9, 16, 25 };
+            double[] dataX2 = { 1, 2, 3, 4, 5 };
+            double[] dataY2 = { 1, 6, 11, 19, 10 };
+            var a = new ScatterPlot(dataX, dataY);
+            WpfPlot1.Plot.AddScatter(dataX, dataY, System.Drawing.Color.Aqua, Single.Epsilon, Single.Epsilon, MarkerShape.asterisk, LineStyle.Solid, "DUR");
+            WpfPlot1.Plot.AddScatter(dataX2, dataY2, System.Drawing.Color.Blue, Single.Epsilon, Single.Epsilon, MarkerShape.asterisk, LineStyle.Solid, "DHR");
+            WpfPlot1.Plot.YLabel("Объем товара (единицы)");
+            WpfPlot1.Plot.XLabel("Время (недели)");
+            WpfPlot1.Plot.Legend();
+            WpfPlot1.Refresh();
+        }
+
+        public void FillDiagram(NetworkView diag)
+        {
+           
+            var network = new NetworkViewModel();
+            var node1 = new ConstantNodeViewModel();
+            network.Nodes.Add(node1);
+            var node2 = new LevelNodeModel();
+            network.Nodes.Add(node2);
+            diag.ViewModel = network;
+        }
 
         /*
          *     <Grid>
         <nodenetwork:NetworkView x:Name="networkView" Background="AliceBlue"/>
     </Grid>
          * */
-        private void openNewPage(string name)
+        private void openNewPage(string name, string type)
         {
            int  W = 130;
            int  H = 20;
-
-
-
 
            // networkView.ViewModel = network;
             TabItem page = new TabItem();
@@ -188,18 +207,37 @@ namespace ForresterModeller
             canvas.Children.Add(but);
 
             page.Header = canvas;
-           // page = ;
-           page.Content = "Содержимое файла "+name;
-            OpenPages.Items.Add(page);
 
-         
+            if (type == "diagram")
+            {
+                NetworkView graf = new NetworkView() {Background = Brushes.AliceBlue };
+
+
+                FillDiagram(graf);
+                page.Content = graf;
+            }
+            else if (type == "plotter")
+            {
+
+                WpfPlot plot = new WpfPlot() { Name = "WpfPlot1" };
+                FillPlot(plot);
+                page.Content = plot;
+            }
+            OpenPages.Items.Add(page);
+           
 
 
         }
 
         private void TestPlot(object sender, RoutedEventArgs e)
         {
-            Open_Page(ToolsFrame, new PlotPage());
+            openNewPage("file2", "plotter");
+        }
+        private void TestGraf(object sender, RoutedEventArgs e)
+        {
+            //  Open_Page(ToolsFrame, new PlotPage());
+
+            openNewPage("file1", "diagram");
         }
     }
 }
