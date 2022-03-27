41900cadb3759a1ad697572ed7ad7fc126c0eccf
Верещагина Ирина Александровна <hjr462@mail.ru>
2022-03-27 20:29:32 +0700

    ....

---
+++ ForresterModeller/src/ProjectManager/LoaderClass.cs
@@ -0,0 +1,28 @@
+﻿using ForresterModeller.src.Nodes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForresterModeller.src.ProjectManager
+{
+    class LoaderClass
+    {
+        public static string puth;
+
+        public IForesterModel createModel(string type)
+        {
+            
+   /*         switch (type)
+                {
+                case (LevelNodeModel.GetType().ToString()):
+                    break;
+
+            }*/
+            
+
+            return new LevelNodeModel();
+        }
+    }
+}
