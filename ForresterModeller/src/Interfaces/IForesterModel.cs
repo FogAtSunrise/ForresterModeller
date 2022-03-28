7cdb455311a6c95bf3ba6d12af849682a460b64b
olga <bachisheo@gmail.com>
2022-03-28 19:02:01 +0700

    Интрефейсы вынесены в отдельную папку

---
+++ ForresterModeller/src/Interfaces/IForesterModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using System.Text.Json.Nodes;
+using ForresterModeller.src.Pages.Properties;
+
+namespace ForresterModeller.src.Nodes.Models
+{
+    public interface IForesterModel : IJSONSerializable, IPropertyChangable
+    {
+        public string Description { get; set; }
+        /// <summary>
+        /// Литерал, обозначающий тип узла
+        /// </summary>
+        public string TypeName { get; set; }
+
+        /// <summary>
+        /// Уникальный идентификатор модели
+        /// </summary>
+        public string Id { get; set; }
+
+    }
+}
---
+++ ForresterModeller/src/Interfaces/IPropertyChangable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+using ForresterModeller.src.Pages.Properties;
+
+namespace ForresterModeller.src.Nodes.Models
+{
+    public interface IPropertyChangable
+    {
+        /// <summary>
+        /// Вернуть перечень объектов, которые будут отражены пользователю
+        /// и (если необходимо) изменены им
+        /// </summary>
+        /// <returns></returns>
+        public abstract ObservableCollection<Property> GetProperties();
+    }
+}
