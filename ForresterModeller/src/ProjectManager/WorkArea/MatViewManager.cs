7ee94749294a2c40af920170fd166e7e8d35945d
Верещагина Ирина Александровна <hjr462@mail.ru>
2022-05-13 07:12:19 +0700

    экспорт, но без русских слов

--- ForresterModeller/src/ProjectManager/WorkArea/MatViewManager.cs
+++ ForresterModeller/src/ProjectManager/WorkArea/MatViewManager.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections.ObjectModel;
-using System.Reactive;
-using System.Windows.Controls;
+﻿using System;
 using ForresterModeller.src.Interfaces;
 using ForresterModeller.src.Nodes.Models;
 using ForresterModeller.src.Windows.ViewModels;
 using ForresterModeller.src.Windows.Views;
+using iTextSharp.text.pdf;
 using ReactiveUI;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Reactive;
+using System.Windows.Controls;
+using iTextSharp.text;
 
 namespace ForresterModeller.src.ProjectManager.WorkArea
 {
@@ -16,6 +20,7 @@ namespace ForresterModeller.src.ProjectManager.WorkArea
         public DiagramManager Diagram { get; set; }
 
         public ReactiveCommand<Unit, Unit> UpdateMath { get; }
+        public ReactiveCommand<Unit, Unit> pdfConvert { get; }
 
         public MatViewManager(DiagramManager dmanager)
         {
@@ -37,7 +42,115 @@ namespace ForresterModeller.src.ProjectManager.WorkArea
                 }
 
             UpdateMath = ReactiveCommand.Create<Unit>(u => updatemod());
+            pdfConvert = ReactiveCommand.Create<Unit>(u => getPDF());
 
+        }
+
+        public void getPDF()
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                string path = dlg.FileName;
+
+                 String FONT = "arial.ttf";
+
+                var document = new iTextSharp.text.Document();
+                using (var writer = PdfWriter.GetInstance(document, new FileStream(path+".pdf", FileMode.Create)))
+                {
+                    iTextSharp.text.Font _standardFont = new iTextSharp.text.Font(
+                        iTextSharp.text.Font.FontFamily.HELVETICA, 9, iTextSharp.text.Font.NORMAL, BaseColor.BLACK);
+                    iTextSharp.text.Font CompanyFont = new iTextSharp.text.Font(
+                        iTextSharp.text.Font.FontFamily.HELVETICA, 9, iTextSharp.text.Font.BOLD, BaseColor.BLACK);
+                    iTextSharp.text.Font HeaderGrowerFont = new iTextSharp.text.Font(
+                        iTextSharp.text.Font.FontFamily.HELVETICA, 10, iTextSharp.text.Font.BOLD, BaseColor.BLACK);
+
+                    document.Open();
+
+                    
+                  
+                   
+                    //BaseFont baseFont = BaseFont.CreateFont("arial.ttf", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+
+                    //Header ---------------------------------------------------------------
+
+                    PdfPTable tblHeader = new PdfPTable(1);
+                    tblHeader.WidthPercentage = 100;
+
+
+                    PdfPCell DiagramName =
+                        new PdfPCell(new Phrase(Name, HeaderGrowerFont));
+                    DiagramName.BorderWidth = 0;
+                    DiagramName.HorizontalAlignment = Element.ALIGN_CENTER;
+                    DiagramName.Colspan = 1;
+                    DiagramName.FixedHeight = 40f;
+
+                    tblHeader.AddCell(DiagramName);
+                    document.Add(new Paragraph(" "));
+
+                    foreach (var item in Models)
+                    {
+
+                        PdfPCell str = new PdfPCell(new Phrase(item.NodeForMod.Name, CompanyFont));
+                        str.HorizontalAlignment = Element.ALIGN_LEFT;
+                        str.BorderWidth = 1;
+                        str.Colspan = 1;
+                        str.FixedHeight = 30f;
+
+                        tblHeader.AddCell(str);
+                        document.Add(new Paragraph(" "));
+
+                        str = new PdfPCell(new Phrase("Описание:", CompanyFont));
+
+                     
+
+                        str.BorderWidth = 0;
+                        str.HorizontalAlignment = Element.ALIGN_LEFT;
+                        str.Colspan = 1;
+                        str.FixedHeight = 20f;
+
+                        tblHeader.AddCell(str);
+                        document.Add(new Paragraph(" "));
+
+                        str = new PdfPCell(new Phrase(item.NodeForMod.Description, CompanyFont));
+                        str.HorizontalAlignment = Element.ALIGN_LEFT;
+                        str.BorderWidth = 0;
+                        str.Colspan = 1;
+                        str.FixedHeight = 20f;
+
+                        tblHeader.AddCell(str);
+                        document.Add(new Paragraph(" "));
+
+
+
+                        foreach (var item2 in item.Data)
+                        {
+                            str = new PdfPCell(new Phrase(item2.Left+item2.sim+item2.Right, _standardFont));
+                            str.HorizontalAlignment = Element.ALIGN_LEFT;
+                            str.BorderWidth = 0;
+                            str.Colspan = 1;
+                            str.FixedHeight = 20f;
+
+                            tblHeader.AddCell(str);
+                            document.Add(new Paragraph(" "));
+
+                        }
+
+
+
+                    }
+
+                    document.Add(tblHeader);
+
+                   document.Close();
+                    writer.Close();
+
+                }
+            }
         }
 
         public void updatemod()
